eaaa332cc519a2a94c9ac3889c6cb09613e9c798
Youngtimer190 <youngtimer190@wp.pl>
2026-03-19 15:40:09 +0100

    Delete src directory

--- src/data/initialData.ts
+++
@@ -1,9 +0,0 @@
-import { Transaction, Task, Meal, Vehicle, Pet, Member, ShoppingList } from '../types';
-
-export const initialTransactions: Transaction[] = [];
-export const initialTasks: Task[] = [];
-export const initialMeals: Meal[] = [];
-export const initialVehicles: Vehicle[] = [];
-export const initialPets: Pet[] = [];
-export const initialMembers: Member[] = [];
-export const initialShoppingLists: ShoppingList[] = [];
